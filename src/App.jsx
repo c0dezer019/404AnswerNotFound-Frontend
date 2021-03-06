e451e1f95d5d3e1476295add94c1bde8edad222d
c0dezer019 <c0dezer019@zohomail.com>
2021-03-05 22:00:26 -0500

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routes from './config/routes';
+import 'semantic-ui-css/semantic.min.css';
+
+const App = () => (
+  <div className="App">
+    <Routes />
+  </div>
+);
+
+export default App;
